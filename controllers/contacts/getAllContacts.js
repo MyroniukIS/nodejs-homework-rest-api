0b64d1e97779546cab96f42d10197bc9e45535f4
Myroniuk Ilona <knopkakyrs@gmail.com>
2022-02-10 12:02:35 +0200

    added pagination

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,10 +2,12 @@ const { Contact } = require("../../models/contacts");
 
 const getAllContacts = async (req, res, next) => {
   try {
+    const {page = 1, limit = 20} = req.query;
     const { _id } = req.user;
+    const skip = (page - 1) * limit;
     const result = await Contact.find(
       {owner: _id},
-       "-createdAt -updatedAt").populate("owner", "email");
+       "-createdAt -updatedAt", {skip, limit: +limit}).populate("owner", "email");
     res.json(result);
   } catch (error) {
     next(error);
